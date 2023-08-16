538c79de69112bdf5d86d5ea15acbc4ebcd851e2
Hang <126535378+ThanBichHangg@users.noreply.github.com>
2023-08-16 12:17:36 +0700

    update 3

--- resources/views/admin/layout/logo.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="logo">
-        <h1 class="text-light"><a href="{{ route('product.home') }}"><span>Home</span></a></h1>
-        <!-- Uncomment below if you prefer to use an image logo -->
-        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
-      </div>
